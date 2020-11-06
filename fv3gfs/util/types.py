78b9565cf698ecced2e53ec2fee0b3fe30a8cce5
Jeremy McGibbon <jeremym@vulcan.com>
2020-11-06 09:35:56 -0800

    fix linting

--- fv3gfs/util/types.py
+++ fv3gfs/util/types.py
@@ -1,18 +1,19 @@
-from typing import Iterable, Any, TypeVar, Tuple
-from types import ModuleType
+from typing import Iterable, TypeVar, Tuple
 from typing_extensions import Protocol
 
 Array = TypeVar("Array")
 
-class Allocator(Protocol):
 
+class Allocator(Protocol):
     def __call__(self, shape: Iterable[int], dtype: type) -> Array:
         pass
 
+
 class NumpyModule(Protocol):
-    
+
     empty: Allocator
     zeros: Allocator
     ones: Allocator
-    
-    def rot90(self, m: Array, k: int = 1, axes: Tuple[int, int] = (0, 1)): ...
+
+    def rot90(self, m: Array, k: int = 1, axes: Tuple[int, int] = (0, 1)):
+        ...
