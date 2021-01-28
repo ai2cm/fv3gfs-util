969b77db4a5682688a26201e9101b419762056ff
Noah D. Brenowitz <nbren12@gmail.com>
2021-01-28 13:29:19 -0800

    Relax the scipy version used
    SciPy 1.6 and higher do not support python 3.6 any longer:
    
    https://docs.scipy.org/doc/scipy/reference/release.1.6.0.html

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ requirements = [
     "fsspec>=0.6.0",
     "zarr>=2.3.2",
     "typing_extensions>=3.7.4",
-    "scipy>=1.6.0",
+    "scipy>=1.3.1",
 ]
 if sys.version_info.major == 3 and sys.version_info.minor == 6:
     requirements.append("dataclasses")
